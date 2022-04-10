e94082c905725d47b7d60424a0dc4cabc735130f
shamim0183 <77606340+shamim0183@users.noreply.github.com>
2022-04-10 09:04:27 +0600

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,52 @@
+var chart_data = [
+    { "group_name": "Total TDS Deductions 90,00,000", "name": "Jan", "value": 26037 },{ "group_name": "Total TDS Deductions 90,00,000", "name": "Feb", "value": 26037 },
+    { "group_name": "Total TDS Deductions 90,00,000", "name": "Mar", "value": 26037 },{ "group_name": "Total TDS Deductions 90,00,000", "name": "Apr", "value": 26037 },
+    { "group_name": "Total TDS Deductions 90,00,000", "name": "May", "value": 26037 },{ "group_name": "Total TDS Deductions 90,00,000", "name": "Jun", "value": 26037 },
+    { "group_name": "Total TDS Deductions 90,00,000", "name": "Jul", "value": 26037 },{ "group_name": "Total TDS Deductions 90,00,000", "name": "Aug", "value": 26037 },
+    { "group_name": "Total TDS Deductions 90,00,000", "name": "Sep", "value": 26037 },{ "group_name": "Total TDS Deductions 90,00,000", "name": "Oct", "value": 26037 },
+    { "group_name": "Total TDS Deductions 90,00,000", "name": "Nov", "value": 26037 },{ "group_name": "Total TDS Deductions 90,00,000", "name": "Dec", "value": 26037 },
+    
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Jan", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Feb", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Mar", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Apr", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "May", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Jun", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Jul", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Aug", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Sep", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Oct", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Nov", "value": 33102 },
+    { "group_name": "Total Employees Net Pay 100,00,000", "name": "Dec", "value": 33102 },
+
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Jan", "value": 37530 },    
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Feb", "value": 37530 },    
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Mar", "value": 37530 },    
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Apr", "value": 37530 },    
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "May", "value": 37530 },
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Jun", "value": 37530 },    
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Jul", "value": 37530 },
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Aug", "value": 37530 },    
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Sep", "value": 37530 },
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Oct", "value": 37530 },    
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Nov", "value": 37530 },
+    { "group_name": "Total Payroll Total Cost 200,00,000", "name": "Dec", "value": 37530 },
+    
+   
+    
+    ]
+   
+    $('#chtAnimatedBarChart').animatedBarChart({
+        data: chart_data,
+        legend: {
+           position: LegendPosition.top,
+           width: 150
+        }
+     });
+
+
+
+     
+    
+   
+
